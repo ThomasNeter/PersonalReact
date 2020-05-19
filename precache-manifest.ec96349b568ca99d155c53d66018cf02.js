8f3e9f5362293fe9a3c791c017b697d1bd33bb64
Thomas Neter <neterthomas@gmail.com>
2020-05-19 16:53:00 -0400

    Updates

---
+++ precache-manifest.ec96349b568ca99d155c53d66018cf02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab394529d5bef43f45a3a436075abea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce0525ff631e611c3dc6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a36413b8858d18dbdbe7",
+    "url": "/static/js/2.202a9172.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.202a9172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0525ff631e611c3dc6",
+    "url": "/static/js/main.97cdd1de.chunk.js"
+  },
+  {
+    "revision": "b70d798dc3147c16c694",
+    "url": "/static/js/runtime-main.5578bb5d.js"
+  }
+]);
