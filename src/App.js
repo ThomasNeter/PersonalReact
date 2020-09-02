c9f9856a4786114cc9590f6c3171055b101ea0ca
Thomas Neter <neterthomas@gmail.com>
2020-09-02 15:07:50 -0400

    added Geotab and removed contact component

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
-        <Contact {...this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
